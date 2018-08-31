ad4d5638216251fe010265fca9e50c50317d6918
Aaron Casanova <aaronccasanova@gmail.com>
2018-08-31 11:07:33 -0700

    added examples patterns

--- src/components/AlgsOfSteele/CommonPatternsList.js
+++ src/components/AlgsOfSteele/CommonPatternsList.js
@@ -3,7 +3,8 @@
 
 // Multiple Pointers - Creating pointers or values that correspond to an index or position and move towards the beginning, end or middle based on a certain condition
 
-// Sliding Window
+// Sliding Window - This pattern involves creating a window which can either be an array or number from one position to another. Depending on a certain condition, the window either increases or closes(and a new window is created). Very useful for keeping track of a subset of data in an array / string etc.
+
 // Divide and Conquer
 // Dynamic Programming
 // Greedy Algorithms
@@ -109,3 +110,26 @@ console.log(countUniqueValues([1, 1, 1, 1, 1, 2]));
 console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));
 console.log(countUniqueValues([]));
 console.log(countUniqueValues([-2, -1, -1, 0, 1]));
+
+//* ------ Sliding Window Ex. ------ *//
+
+const maxSubarraySum = (arr, num) => {
+  let maxSum = 0;
+  let tempSum = 0;
+  if (arr.length < num) return null;
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
+};
+
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2));
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4));
+console.log(maxSubarraySum([4, 2, 1, 6], 1));
+console.log(maxSubarraySum([4, 2, 1, 6, 2], 4));
+console.log(maxSubarraySum([], 4));
