983f7fc69a4b7142e02b5e21efe34c505b206297
Aaron Casanova <aaronccasanova@gmail.com>
2018-09-01 10:52:28 -0700

    bs commit

--- src/components/AlgsOfSteele/CommonPatternsList.js
+++ src/components/AlgsOfSteele/CommonPatternsList.js
@@ -5,7 +5,8 @@
 
 // Sliding Window - This pattern involves creating a window which can either be an array or number from one position to another. Depending on a certain condition, the window either increases or closes(and a new window is created). Very useful for keeping track of a subset of data in an array / string etc.
 
-// Divide and Conquer
+// Divide and Conquer - This pattern involves dividing a data set into smaller chunks and then repeating a process with a subset of data. This pattern can tremendously decrease time complexity.
+
 // Dynamic Programming
 // Greedy Algorithms
 // Backtracking
@@ -133,3 +134,5 @@ console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4));
 console.log(maxSubarraySum([4, 2, 1, 6], 1));
 console.log(maxSubarraySum([4, 2, 1, 6, 2], 4));
 console.log(maxSubarraySum([], 4));
+
+//* ------ Divide and Conquer Ex. ------ *//
